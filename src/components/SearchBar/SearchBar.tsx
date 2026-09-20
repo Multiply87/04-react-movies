34c5815608775163be0637d9857f1ed5f632df6d
Aleksandr Kolodiazhniy <kolodiazhniy.aleksandr@privatbank.ua>
2026-09-20 14:06:34 +0300

    HW04

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,3 @@
-import { type SyntheticEvent } from "react";
 import toast from "react-hot-toast";
 import styles from "./SearchBar.module.css";
 
@@ -7,12 +6,8 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const query = (formData.get("query") as string)?.trim();
+  const handleSubmit = (data: FormData) => {
+    const query = (data.get("query") as string)?.trim();
 
     if (!query) {
       toast.error("Please enter a search query.");
@@ -33,7 +28,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
         >
           Powered by TMDB
         </a>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} action={handleSubmit}>
           <input
             className={styles.input}
             type="text"
